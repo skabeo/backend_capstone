8215c0acd9f4db166eca6f2ab75f09f9aef72684
skabeo <spencerokyere95@gmail.com>
2023-08-17 19:57:12 +0000

    Reset to application base

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,6 @@ module BackendCapstone
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
+    # config.api_only = true
   end
 end
