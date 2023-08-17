0d501fd71c202b7334e9299591f32951c1f1c6a9
skabeo <spencerokyere95@gmail.com>
2023-08-17 10:36:10 +0000

    Add default fault to reserve column

--- db/migrate/20230817092912_create_properties.rb
+++ db/migrate/20230817092912_create_properties.rb
@@ -3,7 +3,7 @@ class CreateProperties < ActiveRecord::Migration[7.0]
     create_table :properties do |t|
       t.string :name
       t.string :image
-      t.boolean :reserve
+      t.boolean :reserve, default: false
       t.string :location
       t.integer :price
       t.timestamps
