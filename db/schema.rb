838993f3274b41bee54a8adede61ae13f49b76de
skabeo <spencerokyere95@gmail.com>
2023-08-17 10:42:16 +0000

    Add reserve false migation

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_17_093149) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_17_104024) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "properties", force: :cascade do |t|
     t.string "name"
     t.string "image"
-    t.boolean "reserve"
+    t.boolean "reserve", default: false
     t.string "location"
     t.integer "price"
     t.datetime "created_at", null: false
