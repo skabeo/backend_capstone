a97ccfa9a4389cccfb299498777acc5f9ecfadd2
Juan Diaz <jfdiaza@unal.edu.co>
2023-08-17 06:06:21 -0400

    add validations to models

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,7 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :property
+
+  validates :city, presence: true, length: { maximum: 20 }
+  validates :day_of_visit, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
     has_many :reservations
     has_many :properties,  through: :reservation
+
+    validates :name, presence: true, length: { maximum: 20 }
+    
 end
