1dda9aeed69c844ea83dde72b289ece010631183
skabeo <spencerokyere95@gmail.com>
2023-08-18 06:48:00 +0000

    Modify user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,11 @@ class User < ApplicationRecord
 	has_many :properties,  through: :reservations
 
 	validates :name, presence: true, length: { maximum: 20 }
+  validates :email, format: URI::MailTo::EMAIL_REGEXP
+  enum role: %i[user admin]
+
+  def self.authenticate(email, password)
+    user = User.find_for_authentication(email: email)
+    user&.valid_password?(password) ? user : nil
+  end
 end
