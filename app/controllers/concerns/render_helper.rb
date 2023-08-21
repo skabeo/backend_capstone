45ab93245c1d6bee0dc02230935d9ae494e629b7
skabeo <spencerokyere95@gmail.com>
2023-08-21 16:47:41 +0000

    Minor fix

--- app/controllers/concerns/render_helper.rb
+++ app/controllers/concerns/render_helper.rb
@@ -2,7 +2,9 @@ module RenderHelper
   extend ActiveSupport::Concern
 
   def create_helper(value, message)
-    if value.save
+    if value.id
+      render_success({ message:, data: value }) if value.destroy!
+    elsif value.save
       render_success({ message:, data: value })
     else
       render_error(value.errors)
