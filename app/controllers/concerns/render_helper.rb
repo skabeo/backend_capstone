20901519867610761a8845248698ed9c82a1a0d4
skabeo <spencerokyere95@gmail.com>
2023-08-21 21:59:36 +0000

    Add render helper

--- app/controllers/concerns/render_helper.rb
+++ app/controllers/concerns/render_helper.rb
@@ -1,6 +1,14 @@
 module RenderHelper
   extend ActiveSupport::Concern
 
+  def render_success(data)
+    render json: { success: true, data: }, status: :ok
+  end
+
+  def render_error(error)
+    render json: { success: false, errors: error }, status: :unprocessable_entity
+  end
+
   def create_helper(value, message)
     if value.id
       render_success({ message:, data: value }) if value.destroy!
