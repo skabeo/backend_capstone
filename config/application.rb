91d8487d41c8d7a100f99c074b05a4099deb15cd
skabeo <spencerokyere95@gmail.com>
2023-08-21 22:00:07 +0000

    configure cors

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,11 @@ module BackendCapstone
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     # config.api_only = true
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins /\Ahttp:\/\/localhost:\d+\z/
+        resource '*', headers: :any, methods: :any
+      end
+    end
   end
 end
