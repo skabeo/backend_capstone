071d44f6bbbbe260e6a1b82a26585bd842f5b92c
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-08-21 17:22:02 +0300

    Fix linters

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require_relative '../../concerns/render_helper'
 
 class Api::V1::ReservationsController < ApiController
--- app/controllers/concerns/render_helper.rb
+++ app/controllers/concerns/render_helper.rb
@@ -1,11 +1,11 @@
 module RenderHelper
-    extend ActiveSupport::Concern
-  
-    def create_helper(value, message)
-      if value.save
-        render_success({ message: message, data: value })
-      else
-        render_error(value.errors)
-      end
+  extend ActiveSupport::Concern
+
+  def create_helper(value, message)
+    if value.save
+      render_success({ message:, data: value })
+    else
+      render_error(value.errors)
     end
+  end
 end
