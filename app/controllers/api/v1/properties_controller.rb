416689c353e3a536e4921896d9ea0d40c883f0d2
skabeo <spencerokyere95@gmail.com>
2023-08-21 21:59:05 +0000

    Add create action

--- app/controllers/api/v1/properties_controller.rb
+++ app/controllers/api/v1/properties_controller.rb
@@ -1,6 +1,17 @@
+require_relative '../../concerns/render_helper'
+
 class Api::V1::PropertiesController < ApplicationController
+  include RenderHelper
+
+  skip_before_action :verify_authenticity_token, only: :create
+
   def index
     @properties = Property.all
     render json: @properties
+  end
+
+  def create
+    @new_property = Property.new(name: params[:name], image: params[:image], price: params[:price], location: params[:location])
+    create_helper(@new_property, 'Property created succesfully')
   end
 end
