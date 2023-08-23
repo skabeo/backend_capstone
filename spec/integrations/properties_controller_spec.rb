cbb859d8814ba6d2d42a18b7b03321a1378e9f9d
adeola003 <khadaf3@gmail.com>
2023-08-23 18:25:21 +0100

    second test for list of properties

--- spec/integrations/properties_controller_spec.rb
+++ spec/integrations/properties_controller_spec.rb
@@ -12,6 +12,26 @@ RSpec.describe 'api/v1/properties', type: :request do
     end
   end
 
- 
+  path '/api/v1/properties' do
+    post 'Create a property' do
+      tags 'properties'
+      consumes 'application/json'
+      parameter name: :property, in: :body, schema: {
+        type: :object,
+        properties: {
+          name: { type: :string },
+          image: { type: :string },
+          price: { type: :number },
+          location: { type: :string }
+        },
+        required: %w[name image price location]
+      }
+      response '200', 'property created' do
+        let(:property) { { name: 'Ile Oyo', image: 'https://image.com', rserve: false, price: 100000, location: 'Oyo state' } }
+        let(:Authorization) { 'Bearer lorem' }
+        run_test!
+      end
+    end
+  end
 
 end
